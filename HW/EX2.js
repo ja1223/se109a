adc10efc6d7488b732cb143152f31529415935da
ja1223 <belle.c33@gmail.com>
2020-12-17 19:08:16 +0800

    EX2.js

---
+++ HW/EX2.js
@@ -0,0 +1,27 @@
+function search(a, o) {//a是陣列
+    var n = a.length//n是陣列的長度
+    var from = 0
+    var to = n
+    
+    while (from<=to){
+        var mid = Math.floor((from + to)/2)//求出陣列的中間
+        if (a[mid] === o)
+            return mid
+        else if (o > a[mid]){
+            from = mid + 1
+        }
+        else {// 剩下o < a[mid]的可能
+            to = mid - 1
+        }
+    }
+    return null
+}
+  
+var t = search([1, 3, 4, 6, 7, 8], 4)
+console.log('t=', t)
+var t = search([1, 3, 4, 6, 7, 8], 5)
+console.log('t=', t)
+var t = search([1, 3, 4, 6, 7, 8], 8)
+console.log('t=', t)
+var t = search([1, 3, 4, 6, 7, 8], 9)
+console.log('t=', t)
